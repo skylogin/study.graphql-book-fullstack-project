839e8be7f19141359c20bf96b89b8ece1a5839bd
정찬호_KBS <ch.jung@kbs.co.kr>
2022-12-06 21:43:02 +0900

    waypoint 적용

--- project/web/src/components/film/FilmList.tsx
+++ project/web/src/components/film/FilmList.tsx
@@ -1,10 +1,11 @@
 import { Box, SimpleGrid, Skeleton } from '@chakra-ui/react';
+import { Waypoint } from 'react-waypoint';
 import { useFilmsQuery } from '../../generated/graphql';
 import FilmCard from './FilmCard';
 
 export default function FilmList(): JSX.Element {
   const LIMIT = 6;
-  const { data, loading, error } = useFilmsQuery({
+  const { data, loading, error, fetchMore } = useFilmsQuery({
     variables: {
       limit: LIMIT,
       cursor: 1,
@@ -19,8 +20,20 @@ export default function FilmList(): JSX.Element {
         new Array(6).fill(0).map((x) => <Skeleton key={x} height="400px" />)}
       {!loading &&
         data &&
-        data.films.films.map((film) => (
+        data.films.films.map((film, i) => (
           <Box key={film.id}>
+            {data.films.cursor && i === data.films.films.length - LIMIT / 2 && (
+              <Waypoint
+                onEnter={() => {
+                  fetchMore({
+                    variables: {
+                      limit: LIMIT,
+                      cursor: data.films.cursor,
+                    },
+                  });
+                }}
+              />
+            )}
             <FilmCard film={film} />
           </Box>
         ))}      
