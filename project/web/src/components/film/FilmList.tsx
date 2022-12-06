eaf10d07bb8bd448fd398f71a75b5b4c5190153b
정찬호_KBS <ch.jung@kbs.co.kr>
2022-12-06 20:37:16 +0900

    UI 추가

--- project/web/src/components/film/FilmList.tsx
+++ project/web/src/components/film/FilmList.tsx
@@ -1,10 +1,23 @@
+import { Box, SimpleGrid, Skeleton } from '@chakra-ui/react';
 import { useFilmsQuery } from '../../generated/graphql';
+import FilmCard from './FilmCard';
 
 export default function FilmList(): JSX.Element {
   const { data, loading, error } = useFilmsQuery();
 
-  if(loading) return <p>...loading</p>;
   if(error) return <p>{error.message}</p>;
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return (
+    <SimpleGrid columns={[2, null, 3]} spacing={[2, null, 10]}>
+      {loading &&
+        new Array(6).fill(0).map((x) => <Skeleton key={x} height="400px" />)}
+      {!loading &&
+        data &&
+        data.films.map((film) => (
+          <Box key={film.id}>
+            <FilmCard film={film} />
+          </Box>
+        ))}      
+    </SimpleGrid>
+  );
 }
