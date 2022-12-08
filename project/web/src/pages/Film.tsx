7d97042d2b32e0497e2e3a4debc5341e7e884843
정찬호_KBS <ch.jung@kbs.co.kr>
2022-12-08 21:25:17 +0900

    상세페이지에 명장면 UI 추가

--- project/web/src/pages/Film.tsx
+++ project/web/src/pages/Film.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Text, Spinner } from '@chakra-ui/react';
+import { Box, Text, Spinner } from '@chakra-ui/react';
 import { useFilmQuery } from '../generated/graphql';
 
 import CommonLayout from '../components/CommonLayout';
 import FilmDetail from '../components/film/FilmDetail';
+import FilmCutList from '../components/film-cut/FilmCutList';
 
 
 interface FilmPageParams {
@@ -23,7 +24,12 @@ function Film(): React.ReactElement {
       { error && <Text>페이지를 표시할 수 없습니다.</Text> }
       
       {filmId && data?.film ? (
-        <FilmDetail film={data.film} />
+        <>
+          <FilmDetail film={data.film} />
+          <Box mt={12}>
+            <FilmCutList filmId={data.film.id} />
+          </Box>
+        </>
       ) : (
         <Text>페이지를 표시할 수 없습니다.</Text>
       )}
