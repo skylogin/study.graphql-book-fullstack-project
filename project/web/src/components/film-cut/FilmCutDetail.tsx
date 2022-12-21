a2b91019303d73670401efccc9637a2a81b4cc30
정찬호_KBS <ch.jung@kbs.co.kr>
2022-12-21 17:06:56 +0900

    로그인 사용자에게 좋아요 UI 제공

--- project/web/src/components/film-cut/FilmCutDetail.tsx
+++ project/web/src/components/film-cut/FilmCutDetail.tsx
@@ -6,17 +6,70 @@ import {
   Heading,
   HStack,
   Image,
+  Text,
+  useColorModeValue,
+  useToast,
 } from '@chakra-ui/react';
+import { useMemo } from 'react';
 import { FaHeart } from 'react-icons/fa';
+import {
+  CutDocument,
+  CutQuery,
+  CutQueryVariables,
+  useVoteMutation,
+  useMeQuery,
+} from '../../generated/graphql';
 
 interface MovieCutDetailProps {
   cutImg: string;
   cutId: number;
+  isVoted?: boolean;
+  votesCount?: number;
 }
 export function FilmCutDetail({
   cutImg,
   cutId,
+  isVoted = false,
+  votesCount = 0,
 }: MovieCutDetailProps): JSX.Element {
+  const toast = useToast();
+  const voteButtonColor = useColorModeValue('gray.500', 'gray.400');
+  const [vote, { loading: voteLoading }] = useVoteMutation({ 
+    variables: { cutId },
+    update: (cache, fetchResult) => {
+      const currentCut = cache.readQuery<CutQuery, CutQueryVariables>({
+        query: CutDocument,
+        variables: { cutId },
+      });
+      if (currentCut && currentCut.cut) {
+        if (fetchResult.data?.vote) {
+          cache.writeQuery<CutQuery, CutQueryVariables>({
+            query: CutDocument,
+            variables: { cutId: currentCut.cut.id },
+            data: {
+              __typename: 'Query',
+              ...currentCut,
+              cut: {
+                ...currentCut.cut,
+                votesCount: isVoted
+                  ? currentCut.cut.votesCount -1
+                  : currentCut.cut.votesCount +1,
+                isVoted: !isVoted,
+              },
+            },
+          });
+        }
+      }
+    },
+  });
+
+  const accessToken = localStorage.getItem('access_token');
+  const { data: userData } = useMeQuery({ skip: !accessToken });
+  const isLoggedIn = useMemo(() => {
+    if (accessToken) return userData?.me?.id;
+    return false;
+  }, [accessToken, userData?.me?.id]);
+
   return (
     <Box>
       <AspectRatio ratio={16 / 9}>
@@ -27,7 +80,23 @@ export function FilmCutDetail({
         <Flex justify="space-between" alignItems="center">
           <Heading size="sm">{cutId} 번째 사진</Heading>
           <HStack spacing={1} alignItems="center">
-            <Button aria-label="like-this-cut-button" leftIcon={<FaHeart />} />
+            <Button
+              color={isVoted ? 'pink.400'  : voteButtonColor} 
+              aria-label="like-this-cut-button" 
+              leftIcon={<FaHeart />}
+              isLoading={voteLoading}
+              onClick={() => {
+                if (isLoggedIn) vote();
+                else {
+                  toast({
+                    status: 'warning',
+                    description: '좋아요 표시는 로그인한 이후 가능합니다.',
+                  });
+                }
+              }} 
+            >
+              <Text>{votesCount}</Text>
+            </Button>
             <Button colorScheme="teal">감상남기기</Button>
           </HStack>
         </Flex>
