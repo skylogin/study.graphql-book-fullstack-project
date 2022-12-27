573eb27ff4552b34a7d4e03eedb41b6d2f5a7c8a
정찬호_KBS <ch.jung@kbs.co.kr>
2022-12-27 10:00:21 +0900

    noti 리졸버 추가

--- project/server/src/apollo/createSchema.ts
+++ project/server/src/apollo/createSchema.ts
@@ -2,6 +2,7 @@ import { GraphQLSchema } from 'graphql';
 import { PubSub } from 'graphql-subscriptions';
 import { buildSchema } from 'type-graphql';
 
+import { NotificationResolver } from '../resolvers/Notification';
 import { FilmResolver } from '../resolvers/Film';
 import { CutResolver } from '../resolvers/Cut';
 import { UserResolver } from '../resolvers/User';
@@ -9,7 +10,13 @@ import { CutReviewResolver } from '../resolvers/CutReview';
 
 export const createSchema = async (): Promise<GraphQLSchema> => {
   return buildSchema({
-    resolvers: [FilmResolver, CutResolver, UserResolver, CutReviewResolver],
+    resolvers: [
+      FilmResolver,
+      CutResolver,
+      UserResolver,
+      CutReviewResolver,
+      NotificationResolver,
+    ],
     pubSub: new PubSub(),
   });
 };
--- project/server/src/resolvers/Notification.ts
+++ project/server/src/resolvers/Notification.ts
@@ -1,11 +1,15 @@
 import {
-  Ctx, 
+  Ctx,
   Int,
   Arg,
   Mutation,
   Query,
   Resolver,
-  UseMiddleware
+  UseMiddleware,
+  Subscription,
+  Root,
+  PubSub,
+  Publisher,
 } from 'type-graphql';
 
 import { MyContext } from '../apollo/createApolloServer';
@@ -13,8 +17,15 @@ import { isAuthenticated } from '../middlewares/isAuthenticated';
 
 import Notification from '../entities/Notification';
 
+const NOTIFICATION_CREATED = 'NOTIFICATION_CREATED';
+
 @Resolver(Notification)
 export class NotificationResolver {
+  @Subscription({ topics: NOTIFICATION_CREATED })
+  newNotification(@Root() notificationPayload: Notification): Notification {
+    return notificationPayload;
+  }
+
   @Query(() => [Notification], {
     description: '세션에 해당되는 유저의 모든 알림을 가져옵니다.',
   })
@@ -34,11 +45,13 @@ export class NotificationResolver {
   async createNotification(
     @Arg('userId', () => Int) userId: number,
     @Arg('text') text: string,
+    @PubSub(NOTIFICATION_CREATED) publish: Publisher<Notification>,
   ): Promise<Notification> {
     const newNoti = await Notification.create({
       text,
       userId,
     }).save();
+    await publish(newNoti);
     return newNoti;
   }
 }
